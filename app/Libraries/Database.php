389e44e76b91111cf26b47d18e26c4dbd3f3f733
Lucas Oliveira <lucasoliveira719@gmail.com>
2021-10-21 10:11:03 -0300

    Conexão com DB

---
+++ app/Libraries/Database.php
@@ -0,0 +1,35 @@
+<?php
+
+class Database{
+
+    private $host ='localhost';
+    private $usuario ='root';
+    private $senha ='';
+    private $banco ='royalfrutas';
+    private $porta ='3306';
+    private $dbh;
+
+
+    public function __construct(){
+        //Fonte de dados contem as informaçoes necessarias para conectar ao banco de dados.
+        $dsn = 'mysql:host='.$this-> host. ';port='.$this->porta.';dbname'.$this->banco;
+        $opcoes = [
+            //Armazena em cache a conexão para ser reutilizada, evita a sobrecarga de uma nova conexão, resultando em um aplicação mais rapida
+            PDO:: ATTR_PERSISTENT => true,
+            //lança uma PDOException se ocorrer um erro
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ];
+
+        try{
+            //Aqui cria a instancia do PDO
+            $this->dbh = new PDO($dsn, $this->usuario, $this->senha, $opcoes);
+
+        }catch(PDOException $e){
+            print "Error:" .$e->getMessage() . "<br/>";
+            die();
+        }
+    }
+}
+
+
+?>
