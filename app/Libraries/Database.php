b6c2e23bfcfc635cc263b39a35f7f68d4ed6b119
Erik Rodrigues <erikdenisrs97@gmail.com>
2021-11-02 01:18:38 -0300

    Created Dockerfile

--- app/Libraries/Database.php
+++ app/Libraries/Database.php
@@ -2,16 +2,16 @@
 
 class Database {
 
-  private $host ='localhost';
-  private $usuario ='root';
-  private $senha ='';
-  private $banco ='royalfrutas';
-  private $porta ='3306';
+  private $host='eunelson-db';
+  private $user='root';
+  private $password='erik';
+  private $database='projeto_integrador';
+  private $port='3306';
 
   public function __construct() {
         
     //Fonte de dados contem as informaçoes necessarias para conectar ao banco de dados.
-    $dsn = 'mysql:host='.$this-> host. ';port='.$this->porta.';dbname'.$this->banco;
+    $dsn = 'mysql:host='.$this-> host. ';port='.$this->port.';dbname'.$this->database;
 
     $opcoes = [
       //Armazena em cache a conexão para ser reutilizada, evita a sobrecarga de uma nova conexão, resultando em um aplicação mais rapida
@@ -22,7 +22,7 @@ class Database {
 
     try {
       //Aqui cria a instancia do PDO
-      $this->dbh = new PDO($dsn, $this->usuario, $this->senha, $opcoes);
+      $this->dbh = new PDO($dsn, $this->user, $this->password, $opcoes);
     }catch(PDOException $e) {
       print "Error:" .$e->getMessage() . "<br/>";
       die();
